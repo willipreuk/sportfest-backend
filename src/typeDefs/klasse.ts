82713a06b1d9e8f1c071a4b4db668525d5bf257b
Willi Preuk <willi.preuk@hnb-holding.com>
2019-12-09 17:08:44 +0100

    klasse

---
+++ src/typeDefs/klasse.ts
@@ -0,0 +1,20 @@
+import { gql } from 'apollo-server';
+
+export default gql`
+    type Klasse {
+        id: Int!
+        stufe: Int!
+        name: Int!
+    }
+
+    extend type Query {
+        allklassen: [Klasse!]!
+        klasse(id: Int!): Klasse
+    }
+
+    extend type Mutation {
+        addKlasse(stufe: Int!, name: Int!): Klasse!
+        deleteKlasse(id: Int!): Klasse
+        updateKlasse(stufe: Int, klasse: Int): Klasse
+    }
+`;
