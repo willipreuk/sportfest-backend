e0580d42c71592e5f9767ba4b2f045a2bd05e68b
Willi Preuk <willi.preuk@hnb-holding.com>
2019-12-09 17:30:21 +0100

    schueler

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { ApolloServer } from 'apollo-server';
 import { initDB } from './database';
 import user from './typeDefs/user';
-
+import klasse from './typeDefs/klasse';
+import schueler from './typeDefs/schueler';
 
 initDB();
 
-const server = new ApolloServer({ typeDefs: [user] });
+const server = new ApolloServer({ typeDefs: [user, klasse, schueler] });
 
 // eslint-disable-next-line no-console
 server.listen().then(({ url }) => console.log(`🚀  Server ready at ${url}`));
---
+++ src/typeDefs/schueler.ts
@@ -0,0 +1,28 @@
+import { gql } from 'apollo-server';
+
+export default gql`
+    enum Geschlecht {
+        m
+        w
+    }
+
+    type Schueler {
+        id: Int!
+        vorname: String!
+        nachname: String!
+        geschlecht: Geschlecht!
+        idklasse: Int!
+    }
+
+    extend type Query {
+        allSchueler(idklasse: Int): [Schueler!]
+        schueler(id: Int!): Schueler
+    }
+
+    extend type Mutation {
+        addSchueler(vorname: String!, nachname: String!, geschlecht: Geschlecht, idklasse: Int!): Schueler!
+        deleteSchueler(id: Int!): Schueler
+        updateSchueler(id: Int!, vorname: String, nachname: String, geschlecht: Geschlecht, idklasse: Int): Schueler
+    }
+
+`;
