529143fc0d56318049e316edd0e6b725ae065c36
Willi Preuk <willi.preuk@hnb-holding.com>
2019-12-09 17:39:36 +0100

    massstaebe

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import { initDB } from './database';
 import user from './typeDefs/user';
 import klasse from './typeDefs/klasse';
 import schueler from './typeDefs/schueler';
+import massstaebe from './typeDefs/massstaebe';
 
 initDB();
 
-const server = new ApolloServer({ typeDefs: [user, klasse, schueler] });
+const server = new ApolloServer({ typeDefs: [user, klasse, schueler, massstaebe] });
 
 // eslint-disable-next-line no-console
 server.listen().then(({ url }) => console.log(`🚀  Server ready at ${url}`));
