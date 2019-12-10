37a3998fe4929f976ffa8036392022b53b0c5ba8
Willi Preuk <willi.preuk@hnb-holding.com>
2019-12-10 17:33:05 +0100

    user resolver

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,14 +1,60 @@
 import bcrypt from 'bcrypt';
+import { UserInputError } from 'apollo-server';
 
 export default {
   Query: {
     allUser: async (obj, args, { db }) => {
-      const [rows] = await db.query('SELECT * FROM user');
+      const [rows] = await db.query('SELECT iduser AS id, username, rolle FROM user');
       return rows;
     },
-    user: async (obj, { username }, { db }) => {
-      const [rows] = await db.execute('SELECT * FROM user WHERE username = ? LIMIT 1', [username]);
+    user: async (obj, { id, username }, { db }) => {
+      if (username) {
+        const [rows] = await db.execute('SELECT iduser AS id, username, rolle FROM user WHERE username = ?', [username]);
+        return rows[0];
+      }
+      const [rows] = await db.execute('SELECT iduser AS id, username, rolle FROM user WHERE iduser = ?', [id]);
       return rows[0];
+    },
+  },
+  Mutation: {
+    addUser: async (obj, args, { db }) => {
+      try {
+        const user = args;
+        user.password = await bcrypt.hash(args.password, 12);
+        const [res] = await db.query('INSERT INTO user SET ?', args);
+        delete user.password;
+        return { ...user, id: res.insertId };
+      } catch (e) {
+        if (e.code === 'ER_DUP_ENTRY') {
+          throw new UserInputError(e.code);
+        } else {
+          throw e;
+        }
+      }
+    },
+    deleteUser: async (obj, { id }, { db }) => {
+      const [rows] = await db.query('DELETE FROM user WHERE iduser = ?', [id]);
+      if (rows.affectedRows > 0) {
+        return { id };
+      }
+      throw new UserInputError('NOT_FOUND');
+    },
+    updateUser: async (obj, args, { db }) => {
+      const newUser = { ...args };
+      delete newUser.id;
+
+      const { password } = newUser;
+      if (password) {
+        newUser.password = await bcrypt.hash(password, 12);
+      }
+
+      const [rows] = await db.query('UPDATE user SET ? WHERE iduser = ?', [newUser, args.id]);
+      if (rows.affectedRows < 1) {
+        throw new UserInputError('NOT_FOUND');
+      }
+
+      const [res] = await db.query('SELECT iduser, username, rolle, iduser AS id  FROM user WHERE iduser = ? ', [args.id]);
+      return res[0];
     },
   },
 };
--- src/typeDefs/user.js
+++ src/typeDefs/user.js
@@ -8,19 +8,36 @@ export default gql`
     }
 
     type User {
+        id: Int!
         username: String!
         password: String!
         rolle: Rolle!
     }
+    
+    type CreateUserPayload {
+        username: String!
+        rolle: String!
+        id: Int!
+    }
+    
+    type DeleteUserPayload {
+        id: Int!
+    }
+    
+    type UpdateUserPayload {
+        id: Int!
+        username: String!
+        rolle: String!
+    }
 
     type Query {
         allUser: [User!]
-        user(username: String!): User
+        user(username: String, id: Int): User
     }
 
     type Mutation {
-        addUser(username: String!, password: String!, rolle: Rolle!): User!
-        deleteUser(rolle: Rolle): User
-        updateUser(username: String!, password: String!, rolle: Rolle!): User
+        addUser(username: String!, password: String!, rolle: Rolle!): CreateUserPayload!
+        deleteUser(id: Int!): DeleteUserPayload!
+        updateUser(id: Int!, username: String, password: String, rolle: Rolle): UpdateUserPayload!
     }
 `;
