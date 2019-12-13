490041174c9860b1fc932ef4f84485dfbd0dbee1
Willi Preuk <willi.preuk@hnb-holding.com>
2019-12-13 21:22:57 +0100

    create pool connection

--- src/database.js
+++ src/database.js
@@ -1,13 +1,15 @@
 import mysql from 'mysql2';
 
 export default () => {
-  let connection = mysql.createConnection({
+  let connection = mysql.createPool({
     host: 'localhost',
     user: 'sportfest-DEV',
     password: 'password',
     database: 'sportfestDEV',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
   });
-  connection.connect();
   connection = connection.promise();
   return connection;
 };
