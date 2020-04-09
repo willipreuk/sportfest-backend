b6a70967dc8258c3f546b41198356e2ff7218d83
willi.preuk <willi@preuk.eu>
2020-04-09 20:12:06 +0200

    added durchschnitt to types

--- src/typeDefs/auswertung.js
+++ src/typeDefs/auswertung.js
@@ -13,6 +13,11 @@ export default gql`
         punkte: Int!
         ergebnisse: [Auswertung!]
     }
+
+    type AuswertungKlasse {
+        schueler: [AuswertungSchueler!]
+        durchschnitt: Float!
+    }
     
     type AuswertungStufe {
         bestM: [AuswertungSchueler!]!
@@ -22,10 +27,11 @@ export default gql`
     type AuswertungStufen {
         bestM: [AuswertungSchueler!]!
         bestW: [AuswertungSchueler!]!
+        besteKlassen: [AuswertungKlasse!]!
     }
   
     extend type Query {
-        auswertungKlasse(id: Int!): [AuswertungSchueler!]
+        auswertungKlasse(id: Int!): AuswertungKlasse
         auswertungSchueler(id: Int!): AuswertungSchueler
         auswertungStufe(stufe: Int!): AuswertungStufe
         auswertungStufen(von: Int!, bis: Int!): AuswertungStufen
